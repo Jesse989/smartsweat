8c5608bd2f9a6e719bc29a632a254dd57608288a
Jesse <jesseneumann@gmail.com>
2024-08-07 15:16:52 -0600

    feat(app): add loading pages to improve ux

--- app/(private)/profile/loading.tsx
+++ app/(private)/profile/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton, Stack } from '@mui/joy';
 export default function Loading() {
   return (
     <Stack minHeight="100%" bgcolor="background.surface" gap={3}>
-      <Stack alignItems="center" gap={2}>
+      <Stack alignItems="center" gap={0.5} py={1}>
         <Skeleton variant="rectangular" height="36px" width="340px" />
         <Skeleton variant="rectangular" height="36px" width="100%" />
       </Stack>
